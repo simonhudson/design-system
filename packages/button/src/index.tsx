4849eb9921caedf747dcdd93d83c9f580d7becaf
Simon Hudson <hellosimonhudson@gmail.com>
2024-01-04 10:01:53 +0000

    initial commit

---
+++ packages/button/src/index.tsx
@@ -0,0 +1,43 @@
+import { MouseEvent, ReactNode } from 'react';
+import { StyledButtonRow, StyledButton } from './index.styles';
+
+export interface ButtonProps {
+	href?: string;
+	isDestructive?: boolean;
+	isWide?: boolean;
+	label: string;
+	onClick?: (e: MouseEvent) => void;
+	opensNewTab?: boolean;
+	size?: 'small' | 'medium' | 'large' | 'x-large';
+	variant?: 'primary' | 'secondary' | 'tertiary';
+}
+
+export const Button = ({
+	href,
+	isDestructive = false,
+	isWide = false,
+	label,
+	onClick,
+	opensNewTab = false,
+	size = 'medium',
+	variant = 'primary',
+}: ButtonProps) => {
+	return (
+		<StyledButton
+			as={href ? 'a' : null}
+			href={href || null}
+			data-is-destructive={isDestructive}
+			data-is-wide={isWide}
+			onClick={(e: MouseEvent) => {
+				if (onClick) onClick(e);
+			}}
+			data-size={size}
+			target={href && opensNewTab ? '_blank' : null}
+			data-variant={variant}
+		>
+			{label}
+		</StyledButton>
+	);
+};
+
+export const ButtonRow = ({ children }: { children: ReactNode }) => <StyledButtonRow>{children}</StyledButtonRow>;
